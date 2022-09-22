828b21e4e4c8f3f54ee2e5aacdc92d2fc8c29b78
Vitória Piloto <57605180+VitoriaPiloto@users.noreply.github.com>
2022-09-21 21:38:23 -0300

    Add files via upload

---
+++ Aula 6 - Inserindo alunos com o banco de dados/Deleta_AlunosBD.php
@@ -0,0 +1,33 @@
+<?php
+    $servidor="localhost";
+    $usuario = "root";
+    $senha = "";
+    $bancodeDados = "3daw_acad_manha";
+
+       if($_SERVER["REQUEST_METHOD"] == "POST"){
+        $mat = $_POST["matricula"];
+
+        $conn = new mysqli($servidor, $usuario, $senha, $bancodedados);
+        if($conn->connect_error){
+            die("Conexao com banco de dados falhou.");
+        }
+
+        $sql = "DELETE FROM `alunos` WHERE `matricula` = $mat";
+        $result = $conn->query($sql);
+    }
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+        <meta charset="UTF-8">
+        <title>Document</title>
+</head>
+<body>
+    <h1>Deletar Alunos</h1>
+        <form action="Deleta_AlunosBD.php" method="POST">
+            <label for="matricula">Matricula do Aluno</label><br>
+            <input type="text" name="matricula"><br>
+            <input type="submit" value="Enviar">
+        </form>
+    </body>
+</body>
