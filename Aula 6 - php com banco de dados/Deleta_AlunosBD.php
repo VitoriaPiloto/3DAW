36e9d153a0a4b1423fb4ede65880dceca283b0e7
Vitória <vitoriapiloto477@gmail.com>
2022-09-26 20:55:05 -0300

    atualizando o estilo da página deletar alunos

--- Aula 6 - php com banco de dados/Deleta_AlunosBD.php
+++ Aula 6 - php com banco de dados/Deleta_AlunosBD.php
@@ -21,13 +21,71 @@
 <head>
         <meta charset="UTF-8">
         <title>Document</title>
+        <style>
+            body{
+            padding-right:25%;
+            padding-left:25%;
+            padding-top: 2%;
+            text-transform: uppercase;
+            font-family: 'Poppins', sans-serif;
+            background-color: beige;
+        }
+        h1{
+            color: #E82901;
+            text-align: center;
+        }
+        input{
+            background-color: beige;
+            padding: 5px;
+            border: 1px solid black;
+            border-radius: 10px;
+            font-family: 'Poppins', sans-serif;
+            margin-top: 5px;
+            width: 70%;
+        }
+
+        label, input{
+            margin-left: 5vw;
+        }
+        label{
+            margin-top: 5vh;
+        }
+        fieldset{
+            display: grid;
+            border: 1px solid #5C1000;
+        }
+
+        legend{
+            margin-left: 2.5vw;
+            margin-right: 2.5vw;
+            font-size: large;
+            color:#5C1000;
+        }
+
+        #button{
+            width: 30%;
+            margin-top: 5vh;
+            padding: 10px;
+            background-color: #E82901;
+            border:none;
+            color:beige;
+        }
+        #button:hover{
+            cursor: pointer;
+            background-color: #5C1000;
+        }
+        </style>
 </head>
 <body>
     <h1>Deletar Alunos</h1>
         <form action="Deleta_AlunosBD.php" method="POST">
-            <label for="matricula">Matricula do Aluno</label><br>
-            <input type="text" name="matricula"><br>
-            <input type="submit" value="Enviar">
+            <fieldset>
+                <legend>Insira a matrícula do aluno que deseja deletar</legend>
+                <label for="matricula">Matricula do Aluno</label><br>
+                <input type="text" name="matricula"><br>
+                <input type="submit" value="Enviar" id="button">
+            </fieldset>
+            
         </form>
     </body>
 </body>
