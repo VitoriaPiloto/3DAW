75e2fc861510e773355404ff0e816608e972b557
Vitória <vitoriapiloto477@gmail.com>
2022-09-26 20:51:09 -0300

    atualizando estilo página alteração de alunos

--- Aula 6 - php com banco de dados/Altera_AlunosBD.php
+++ Aula 6 - php com banco de dados/Altera_AlunosBD.php
@@ -40,30 +40,88 @@
 <head>
         <meta charset="UTF-8">
         <title>Document</title>
+        <style>
+            @import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
+        body{
+            padding-right:25%;
+            padding-left:25%;
+            padding-top: 2%;
+            text-transform: uppercase;
+            font-family: 'Poppins', sans-serif;
+            background-color: beige;
+        }
+        h1{
+            color: #E82901;
+            text-align: center;
+        }
+        h2{
+            color: #5C1000;  
+        }
+        input{
+            background-color: beige;
+            padding: 5px;
+            border: 1px solid black;
+            border-radius: 10px;
+            font-family: 'Poppins', sans-serif;
+            margin-top: 5px;
+            width: 70%;
+        }
+
+        label, input{
+            margin-left: 5vw;
+        }
+
+        fieldset{
+            display: grid;
+            border: 1px solid #5C1000;
+        }
+
+        legend{
+            margin-left: 2.5vw;
+            margin-right: 2.5vw;
+            font-size: large;
+            color:#5C1000;
+        }
+
+        #button{
+            width: 30%;
+            margin-top: 5vh;
+            padding: 10px;
+            background-color: #E82901;
+            border:none;
+            color:beige;
+        }
+        #button:hover{
+            cursor: pointer;
+            background-color: #5C1000;
+        }
+        </style>
 </head>
 <body>
     <h1> Alterar Alunos </h1>
-    <h2>Preencha o dado antigo e o dado novo</h2>
     <form action="Altera_AlunosBD.php" method="POST">
         <fieldset>
+            <legend>Preencha o dado antigo e o dado novo</legend>
             <label for="nome">Nome: </label>
-            <input type="text" id="nome" name="nome"><br><br>
+            <input type="text" id="nome" name="nome">
 
             <label for="nomeNovo">Nome Novo: </label>
-            <input type="text" id="nomeNovo" name="nomeNovo"><br><br>
+            
+            <input type="text" id="nomeNovo" name="nomeNovo">
 
             <label for="matricula">Matricula : </label>
-            <input type="text" id="matricula" name="matricula"><br><br>
+            <input type="text" id="matricula" name="matricula">
 
             <label for="matriculaNova">Matricula Nova: </label>
-            <input type="text" id="matriculaNova" name="matriculaNova"><br><br>
+            <input type="text" id="matriculaNova" name="matriculaNova">
 
             <label for="email">Email : </label>
-            <input type="text" id="email" name="email"><br><br>
+            <input type="text" id="email" name="email">
 
             <label for="emailNovo">Email Novo: </label>
-            <input type="text" id="emailNovo" name="emailNovo"><br><br>
-            <input type="submit" value="Enviar">
+            <input type="text" id="emailNovo" name="emailNovo">
+
+            <input type="submit" value="Enviar" id="button">
         </fieldset>
     </form>
 </body>
